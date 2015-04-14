039ed5e80bbe2724f8bc22080ea5928881ab0b66
renan.ernesto <renan.ernesto@vdi-unijava-001.intranet.ciss>
2015-04-14 17:05:18 -0300

    construindo classe pesquisa

---
+++ src/br/com/uniciss/imobiliaria/classes/Pesquisa.java
@@ -0,0 +1,17 @@
+package br.com.uniciss.imobiliaria.classes;
+
+public class Pesquisa {
+
+	void PesquisaCliente() {
+
+	}
+
+	void PesquisaFuncionarios() {
+
+	}
+
+	void PesquisaImoveis() {
+
+	}
+
+}

062966c560bb74538f969cec67b89936cc12b5fc
fabio.borba <fabio.borba@vdi-unijava-014.intranet.ciss>
2015-04-15 16:28:07 -0300

    Cadastro imoveis + alteração ler banco

--- src/br/com/uniciss/imobiliaria/classes/LerBanco.java
+++ src/br/com/uniciss/imobiliaria/classes/LerBanco.java
@@ -8,14 +8,13 @@ import java.util.Map;
 public class LerBanco {
 	public void leituraUsuario(Map<String, Usuario> listaUsuario) {
 		try {
-			
+
 			FileReader arq = new FileReader("Usuarios.txt");
 			BufferedReader lerArq = new BufferedReader(arq);
 			String linha = lerArq.readLine();
 
-			
 			while (linha != null) {
-			
+
 				String palavras[] = linha.split(",");
 				// Instanciacao de um novo usuario
 				Usuario p = new Usuario();
@@ -35,4 +34,37 @@ public class LerBanco {
 		}
 
 	}
+
+	public void leituraImovel(Map<Integer, Imovel> listaImovel) {
+		try {
+
+			FileReader arq = new FileReader("Imoveis.txt");
+			BufferedReader lerArq = new BufferedReader(arq);
+			String linha = lerArq.readLine();
+
+			while (linha != null) {
+
+				String palavras[] = linha.split(",");
+				// Instanciacao de um novo usuario
+				Imovel p = new Imovel();
+
+				// Passa a ordem de cada campo
+				p.setEndereco(palavras[1]);
+				p.setTamanhoImovel(palavras[2]);
+				p.setNumeroDeComodos(palavras[3]);
+				p.setValorImovel(Integer.parseInt(palavras[4]));
+				p.setTipo(palavras[5]);
+				p.setStatus(palavras[6]);
+
+				listaImovel.put(p.getCod(), p);
+				linha = lerArq.readLine();
+			}
+			arq.close();
+		} catch (IOException e) {
+			System.err.printf("Erro na abertura do arquivo: %s.\n",
+					e.getMessage());
+		}
+
+	}
+
 }

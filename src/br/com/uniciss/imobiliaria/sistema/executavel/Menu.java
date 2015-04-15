177144727b4e17262f3a7ccc6366ffe0abba5e04
caroline.adami <caroline.adami@vdi-unijava-006.intranet.ciss>
2015-04-15 14:38:39 -0300

    Menu ADM, Secretario e Corretor

--- src/br/com/uniciss/imobiliaria/sistema/executavel/Menu.java
+++ src/br/com/uniciss/imobiliaria/sistema/executavel/Menu.java
@@ -22,13 +22,88 @@ public class Menu {
 	Usuario u = new Usuario();
 	LerBanco ib = new LerBanco();
 	
-	public void menuAdmin(){
+	public void menuAdmin()throws IOException{
+		int opc;
+		System.out.println("ADMINISTRADOR");
 		
+		System.out.println("1 - Cadastrar Secretária");
+		System.out.println("2 - Cadastrar Corretor");
+		System.out.println("3- Sair");
+		s = new Scanner(System.in);
+		opc = s.nextInt();
+		switch(opc){
+		case 1:		
+			
+			break;
+		case 2:
+			
+			break;
+		
+		case 3:		
+			System.out.println("Deseja voltar ao menu principal?");
+			System.out.println("1-Sim");
+			System.out.println("2-Não");
+			opc = s.nextInt();
+			switch(opc){
+			case 1:
+				Menu m = new Menu();
+				m.Login();
+					
+				break;
+			case 2:
+				System.exit(0); 
+			break;
+			
+			
+			default:
+			System.out.println("Opção inválida.");
+			}
+		}
 	}
 	
-	public void menuCorretor(){
+	public void menuCorretor() throws IOException{
+			int opc;
+			System.out.println("CORRETOR");
+			
+			System.out.println("1 - Cadastrar Imóvel Vendido");
+			System.out.println("2 - Cadastrar Imóvel Alugado");
+			System.out.println("3 - Listar Visitas Agendadas");
+			System.out.println("4- Sair");
+			s = new Scanner(System.in);
+			opc = s.nextInt();
+			switch(opc){
+			case 1:		
+				
+				break;
+			case 2:
+				
+				break;
+			
+			case 3:		
+				
+				break;
+			case 4:
+				System.out.println("Deseja voltar ao menu principal?");
+				System.out.println("1-Sim");
+				System.out.println("2-Não");
+				opc = s.nextInt();
+				switch(opc){
+				case 1:
+					Menu m = new Menu();
+					m.Login();
+						
+					break;
+				case 2:
+					System.exit(0); 
+				break;
+				
+				
+				default:
+				System.out.println("Opção inválida.");
+					}
+				}
+		}
 		
-	}
 	
 	public void menuSecretario() throws IOException{
 		int opc;
@@ -44,18 +119,26 @@ public class Menu {
 		s = new Scanner(System.in);
 		opc = s.nextInt();
 		switch(opc){
-		case 1:
-			
-			
-			
-			
+		case 1:		
 			
 			break;
 		case 2:
 			
 			break;
-case 7:
-	System.out.println("Deseja voltar ao menu principal?");
+		case 3:		
+			
+			break;
+		case 4:		
+			
+			break;
+		case 5:		
+			
+			break;
+		case 6:		
+	
+			break;
+		case 7:
+			System.out.println("Deseja voltar ao menu principal?");
 			System.out.println("1-Sim");
 			System.out.println("2-Não");
 			opc = s.nextInt();

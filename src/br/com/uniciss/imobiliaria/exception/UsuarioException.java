d32d69af132b157bda8972b97afaf6455030c354
igor.colaca <igorzao_colacinha@hotmail.com>
2015-04-17 14:48:42 -0300

    //////

--- src/br/com/uniciss/imobiliaria/exception/UsuarioException.java
+++ src/br/com/uniciss/imobiliaria/exception/UsuarioException.java
@@ -3,9 +3,9 @@ package br.com.uniciss.imobiliaria.exception;
 import java.io.FileNotFoundException;
 
 public class UsuarioException extends FileNotFoundException {
-	
+//	
 	public UsuarioException(String message){
-		super(message);
+		super(message); 
 	}
 
 }

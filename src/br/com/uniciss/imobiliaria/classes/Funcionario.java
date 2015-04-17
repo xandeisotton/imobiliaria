e9f3ec6da236fbee736d23a96f52f7f2dfa6aac1
renan.ernesto <renan.ernesto@vdi-unijava-001.intranet.ciss>
2015-04-17 14:47:00 -0300

    funcionario

--- src/br/com/uniciss/imobiliaria/classes/Funcionario.java
+++ src/br/com/uniciss/imobiliaria/classes/Funcionario.java
@@ -1,8 +1,11 @@
 package br.com.uniciss.imobiliaria.classes;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
@@ -36,10 +39,28 @@ public class Funcionario extends Pessoa {
 				telefone = scanner.next();
 			} while (validaN(telefone) == false);
 		}
-
+		// String quantidadeNumeros = String.valueOf(telefone);
+		// if (quantidadeNumeros.length() < 8) {
+		// do {
+		// System.out.println("Telefone deve conter no mínimo 8 digitos");
+		// telefone = scanner.next();
+		// } while (quantidadeNumeros.length() < 8);
+		// }
 		System.out
 				.println("Insira o tipo funcionario : 1 - Corretor / 2 - Secretario");
 		u.setTipo(scanner.next());
+		// int opcao = scanner.nextInt();
+		// if (opcao == 1) {
+		// u.setTipo("Corretor");
+		// return;
+		// }
+		// if (opcao == 2) {
+		// u.setTipo("Secretario");
+		// return;
+		// } else {
+		// System.out.println("Opção inválida");
+		//
+		// }
 
 		System.out.println("Insira o CPF :");
 		setCpf(scanner.next());
@@ -66,8 +87,7 @@ public class Funcionario extends Pessoa {
 
 		do {
 			if (linha.contains(cpf)) {
-				System.out
-						.println("Cpf já está cadastrado,\n insira novamente");
+				System.out.println("Cpf já está cadastrado, insira novamente");
 				cpf = scanner.next();
 				achou = true;
 
@@ -79,13 +99,20 @@ public class Funcionario extends Pessoa {
 
 		System.out.println("Insira a senha do funcionario:");
 		u.setSenha(scanner.next());
+		
+
 
 		GravaTxt grava = new GravaTxt();
 		grava.grava("Funcionario.txt", toString());
 		grava.grava("Usuarios.txt", toStringUsuarios());
 
-		codigo = codigo + 1;
+		
 		System.out.println("cadastrado com sucesso");
+
+		System.out.println("1-> Voltar ao menu");
+		System.out.println("2-> Cadastrar outro funcionário");
+		System.out.println("3-> Log Off");
+
 	}
 
 	public String toString() {

50ff7d59c234c5ca025fb5ca66be2b898c6576ae
renan.ernesto <renan.ernesto@vdi-unijava-001.intranet.ciss>
2015-04-20 16:09:08 -0300

    cliente

--- src/br/com/uniciss/imobiliaria/classes/Cliente.java
+++ src/br/com/uniciss/imobiliaria/classes/Cliente.java
@@ -51,28 +51,33 @@ public class Cliente extends Pessoa {
 		return "Cliente," + banco + "," + numConta + "," + codigo + ","
 				+ cadastro + "," + nome + "," + endereco + "," + cpf + ","
 				+ telefone;
-	} 
+	}
 
 	public void CadastroCliente() throws Exception {
 		cadastro = new Scanner(System.in);
 
 		System.out.println("Informe o nome do cliente!");
 		setNome(cadastro.nextLine());
+		if (validaTexto(nome) == false) {
+			do {
+				System.out.println("nome deve conter somente letras");
+				nome = cadastro.nextLine();
+			} while (validaTexto(nome) == false);
+		}
 
 		System.out.println("Informe o endereco do cliente!");
 		setEndereco(cadastro.nextLine());
 
 		System.out.println("Informe o número do CPF do cliente!");
 		setCpf(cadastro.nextLine());
-		//        Validação DE CPF:
-		if (ValidaCpf.isCpf(getCpf())==false){	
+		// Validação DE CPF:
+		if (ValidaCpf.isCpf(getCpf()) == false) {
 			System.out.println("CPF Invalido");
 		}
-		while (ValidaCpf.isCpf(getCpf())== false){
+		while (ValidaCpf.isCpf(getCpf()) == false) {
 			System.out.println("Informe o número do CPF do cliente!");
-			setCpf(cadastro.nextLine());				
-		 }
-		
+			setCpf(cadastro.nextLine());
+		}
 
 		System.out.println("Informe o número do telefone do ciente!");
 		setTelefone(cadastro.nextLine());
@@ -83,97 +88,95 @@ public class Cliente extends Pessoa {
 		System.out.println("Informe o numero da conta do cliente!");
 		setNumConta(cadastro.nextLine());
 
-	
-		
 		GravaTxt g = new GravaTxt();
 		g.grava("Cliente.txt", toString());
 
 		entrada = new Scanner(System.in);
 	}
-	
+
 	public void pesquisaCliente() throws Exception {
-		
+
 		entrada = new Scanner(System.in);
 		boolean achou = false;
 		String linha = "";
 		@SuppressWarnings("unused")
 		String nomeArquivo = "Clientes.txt";
-		
+
 		System.out.println("Informe o código do cliente");
 		String codigo = entrada.next();
-		
+
 		in = new BufferedReader(new FileReader("Cliente.txt"));
-		
-		try{
-			while((linha = in.readLine())!=null){
-				if(linha.contains(codigo)){
+
+		try {
+			while ((linha = in.readLine()) != null) {
+				if (linha.contains(codigo)) {
 					System.out.println(linha);
 					achou = true;
 				}
 			}
-			if(!achou){
+			if (!achou) {
 				System.out.println("Cliente não existente!\n");
 			}
-		}catch (NullPointerException erro) {
+		} catch (NullPointerException erro) {
 			System.out.println("Código inválido!");
 		}
 	}
-	
-/*	ISSO É UM TESTE, FAVOR NAO APAGAR 
- * 
- * public static void preencheMapa() throws IOException {
-		Iterator<Cliente> i = listaCliente().iterator();
 
-		while (i.hasNext()) {
-			Cliente c = i.next();
-			mapaDeClientes.put(c.getNome(), c);
-		}
-	}
-	public static Cliente buscaPorNome(String nomeBusca) throws IOException {
-		preencheMapa();
-		Cliente c = mapaDeClientes.get(nomeBusca);
-		try {
-			String nome = c.getNome();
-			String cpf = c.getCpf();
-			String endereco = c.getEndereco();
-			String telefone = c.getTelefone();
-		
+	/*
+	 * ISSO É UM TESTE, FAVOR NAO APAGAR
+	 * 
+	 * public static void preencheMapa() throws IOException { Iterator<Cliente>
+	 * i = listaCliente().iterator();
+	 * 
+	 * while (i.hasNext()) { Cliente c = i.next();
+	 * mapaDeClientes.put(c.getNome(), c); } } public static Cliente
+	 * buscaPorNome(String nomeBusca) throws IOException { preencheMapa();
+	 * Cliente c = mapaDeClientes.get(nomeBusca); try { String nome =
+	 * c.getNome(); String cpf = c.getCpf(); String endereco = c.getEndereco();
+	 * String telefone = c.getTelefone();
+	 * 
+	 * 
+	 * c = new Cliente(nome, cpf, endereco, telefone); return c;
+	 * 
+	 * } catch (NullPointerException n) { n.getMessage(); return c; } }
+	 */
 
-			c = new Cliente(nome, cpf, endereco, telefone);
-			return c;
-			
-		} catch (NullPointerException n) {
-			n.getMessage();
-			return c;
-		}
-	}*/
-	
-	public void alteraLinha(String palavraAntiga, String palavraNova) throws IOException{
-		
+	public void alteraLinha(String palavraAntiga, String palavraNova)
+			throws IOException {
+
 		String arquivo = "ARQUIVO";
 		String arquivoTmp = "ARQUIVO=Tmp";
-		
+
 		BufferedWriter writer = new BufferedWriter(new FileWriter(arquivoTmp));
 		BufferedReader reader = new BufferedReader(new FileReader(arquivo));
-		
+
 		String linha;
-		while((linha=reader.readLine()) != null){
-			if(linha.contains(palavraAntiga)){
+		while ((linha = reader.readLine()) != null) {
+			if (linha.contains(palavraAntiga)) {
 				linha = linha.replace(palavraAntiga, palavraNova);
 			}
 			writer.write(linha + "\n");
 		}
 		writer.close();
 		reader.close();
-		
+
 		new File(arquivo).delete();
 		new File(arquivoTmp).renameTo(new File(arquivo));
 	}
-	
-	public void exibirClientes(){
-		for(Object a : listaCliente){
+
+	public void exibirClientes() {
+		for (Object a : listaCliente) {
 			System.out.println(a.toString());
 		}
+	}
+
+	public boolean validaN(String txt) {
+		return txt.matches("^[0-9]*$");
+	}
+
+	public boolean validaTexto(String texto) {
+		return texto.matches("[a-z A-Z]+");
+
 	}
 
 }
